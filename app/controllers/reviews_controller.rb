f7dae0003a3b113ef7ea14ff57f80da11f588cb0
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-10-12 14:43:58 +0300

    added reviews controller and their routes plus seeds files

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,14 @@
+class ReviewsController < ApplicationController
+
+    def index
+
+        reviews = Review.all
+        render json: reviews
+    end
+
+    def show
+
+        review = Review.find_by(id: params[:id])
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Review controller
+
+  get '/reviews', to: 'reviews#index'
+  get '/reviews/:id', to: 'reviews#show'
+
 end
